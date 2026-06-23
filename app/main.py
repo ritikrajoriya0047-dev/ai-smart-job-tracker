00eda1c52c89075396e1bb20973e2e4aa76a2819
ritikrajoriya0047-dev <ritikrajoriya0047@gmail.com>
2026-06-23 11:01:51 +0530

    Fix CORS allow all origins

--- app/main.py
+++ app/main.py
@@ -23,7 +23,12 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ritikrajoriya0047-dev.github.io"],
+    allow_origins=[
+        "https://ritikrajoriya0047-dev.github.io",
+        "http://localhost:3000",
+        "http://127.0.0.1:5500",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
